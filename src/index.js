0acafc3cae3506cd5c5de3f71d5a0ebbd6b02c05
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2020-05-31 21:18:14 +0300

    Taking turns

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
@@ -26,12 +27,15 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ 
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
